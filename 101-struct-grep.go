96d4bf87d9b4c35db65ec7d6b7f8f6a88059b30d
sakura1116vg <daisuke_sasaki@voyagegroup.com>
2018-06-21 13:57:12 +0900

    tc

--- 101-struct-grep.go
+++ 101-struct-grep.go
@@ -16,8 +16,9 @@ func (a ExtV) Less(i, j int) bool { return *a[i].Priority < *a[j].Priority }
 func (a ExtV) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 type Ad struct {
-	VendorName *string `json:"vendor_name"`
-	Priority   *int32  `json:"priority"`
+	VendorName  *string  `json:"vendor_name"`
+	Priority    *int32   `json:"priority"`
+	Imptrackers []string `json:"imptrackers"`
 }
 
 func strPtr(s string) *string {
@@ -30,16 +31,19 @@ func intPtr(i int32) *int32 {
 
 func main() {
 	ad1 := &Ad{
-		VendorName: strPtr("softbank"),
-		Priority:   intPtr(20),
+		VendorName:  strPtr("softbank"),
+		Priority:    intPtr(20),
+		Imptrackers: []string{"hoge"},
 	}
 	ad2 := &Ad{
-		VendorName: strPtr("docomo"),
-		Priority:   intPtr(40),
+		VendorName:  strPtr("docomo"),
+		Priority:    intPtr(40),
+		Imptrackers: []string{"hoge"},
 	}
 	ad3 := &Ad{
-		VendorName: strPtr("au"),
-		Priority:   intPtr(30),
+		VendorName:  strPtr("au"),
+		Priority:    intPtr(30),
+		Imptrackers: []string{"hoge"},
 	}
 
 	ext := &Ext{
@@ -49,16 +53,45 @@ func main() {
 	//ads := []*Ad{ad1, ad2, ad3}
 
 	x := ext.getOne()
-	bytes, _ := json.Marshal(x)
+	//ad := &Ad{
+	//	VendorName:  x.VendorName,
+	//	Priority:    x.Priority,
+	//	Imptrackers: x.Imptrackers,
+	//}
+	//r := make([]string, 1)
+	//s := []string{}
+	//s = append(s, "fuga")
+	//s = append(s, x.Imptrackers)
+	x.Imptrackers = append(x.Imptrackers, "fuga")
+
+	//r = append(r, "fuga", x.Imptrackers)
+	y := &Ext{
+		Ads: []*Ad{
+			{
+				VendorName:  x.VendorName,
+				Priority:    x.Priority,
+				Imptrackers: x.Imptrackers,
+			},
+		},
+	}
+	bytes, _ := json.Marshal(y)
 	fmt.Printf("%s", bytes)
 }
 
 // struct配列に対して、Priorityで昇順ソートして、0番目のstructにPriorityを振り直して返す
-func (x *Ext) getOne() *ExtV {
-	one := make(ExtV, 0)
-	sort.Sort(ExtV(x.Ads))
-	x.Ads[0].Priority = intPtr(1)
-	one = append(one, x.Ads[0])
-	return &one
+//func (x *Ext) getOne() *ExtV {
+//	one := make(ExtV, 0)
+//	sort.Sort(ExtV(x.Ads))
+//	x.Ads[0].Priority = intPtr(0)
+//	one = append(one, x.Ads[1])
+//	return &one
+//}
 
+func (x *Ext) getOne() *Ad {
+	//one := make(ExtV, 0)
+	sort.Sort(ExtV(x.Ads))
+	x.Ads[0].Priority = intPtr(0)
+	//one = append(one, x.Ads[1])
+	//return &one
+	return x.Ads[0]
 }
